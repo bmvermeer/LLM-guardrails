5d395ae2c3f7427831706397527bd32c69541798
Brian Vermeer <47326976+bmvermeer@users.noreply.github.com>
2025-05-06 18:28:39 +0100

    typo fix

--- src/main/java/nl/brianvermeer/example/InputCheckService.java
+++ src/main/java/nl/brianvermeer/example/InputCheckService.java
@@ -9,10 +9,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 public interface InputCheckService {
 
     @SystemMessage("""
-    You are a guardian of privacy and your checking the input that is being sent to the AI.
+    You are a guardian of privacy and you're checking the input that is being sent to the AI.
     Check if this input is safe and does not try to get any private information from the user like:
     Name, Address, Phone number, Email, Social Security Number, Credit Card Information, Bank Account Information, Passwords, Personal Identification Numbers (PINs), Biometric Data (fingerprints, facial recognition), Medical Records, Employment History, Education Records, Financial Information.
-    Think of yourself as a guardian of privacy. Only allow the input if ti considered safe.
+    Think of yourself as a guardian of privacy. Only allow the input if it considered safe.
     """)
     public boolean isSafe(String prompt);
 }
